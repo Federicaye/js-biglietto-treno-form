372b2606f040f726eb1d79f6194b867549c4ffb4
Federicaye <gallofederica58@gmail.com>
2024-03-07 17:12:54 +0100

    price

--- resource/script.js
+++ resource/script.js
@@ -4,24 +4,26 @@ let distance = document.getElementById('distance');
 let age = document.getElementById('age');
 let generate = document.getElementById('generate');
 let cancel = document.getElementById('cancel');
-let price = parseInt(distance) * 0.21;
+let price;
 
 generate.addEventListener ('click',
     function () {
         document.getElementById("passenger").innerHTML= username.value;
         document.getElementById("carriage").innerHTML= username;
         document.getElementById("code").innerHTML= username;
-        document.getElementById("price").innerHTML= username;
+        
+
+        let price = parseInt(distance.value) * 0.21;
 
         if (age === "over-65") {
             document.getElementById("price").innerHTML= (parseInt(price) * 0.8).toFixed(2);
             console.log(price * 0.8)
         }
         else if (age === "minorenne") {
-            document.getElementById("price").innerHTML= (price * 0.6).toFixed(2);
+            document.getElementById("price").innerHTML= (parseInt(price * 0.6)).toFixed(2);
         }
         else {
-            document.getElementById("price").innerHTML= (price).toFixed(2);
+            document.getElementById("price").innerHTML= (parseInt(price)).toFixed(2);
         } 
  
     }
